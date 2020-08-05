ec6ae31ddbc6626a0419e598e2b1d34c1827d9e9
Allen Nguyen <allennguyen@lyft.com>
2020-08-05 10:23:20 -0400

    Day 4 Part 2 Solve

--- days/4.go
+++ days/4.go
@@ -13,13 +13,14 @@ import (
 func generatePasswords(low, upper int) int {
 	count := 0
 	for i := low; i <= upper; i++ {
-		if validateAdjDigits(i) && validateNonDecreaseDigits(i) {
+		if validateNonDecreaseDigits(i) && validateLargerAdjDigits(i) {
 			count += 1
 		}
 	}
 	return count
 }
 
+// Part 1
 func validateAdjDigits(candidate int) bool {
 	str := strconv.Itoa(candidate)
 	valid := false
@@ -43,10 +44,39 @@ func validateNonDecreaseDigits(candidate int) bool {
 	for len(str) > 1 {
 		r, size := utf8.DecodeRuneInString(str)
 		next_r, _ := utf8.DecodeRuneInString(str[size:])
-		if int(r - '0') > int(next_r - '0') {
+		if int(r-'0') > int(next_r-'0') {
 			valid = false
 		}
 		str = str[size:]
+	}
+
+	return valid
+}
+
+// Part 2
+func validateLargerAdjDigits(candidate int) bool {
+	str := strconv.Itoa(candidate)
+	current, size := utf8.DecodeRuneInString(str)
+	str = str[size:]
+	count := 1
+	valid := false
+
+	for len(str) > 0 {
+		r, size := utf8.DecodeRuneInString(str)
+		if r == current {
+			count += 1
+		} else {
+			if count == 2 {
+				valid = true
+			}
+			current = r
+			count = 1
+		}
+		str = str[size:]
+	}
+
+	if count == 2 {
+		valid = true
 	}
 
 	return valid
